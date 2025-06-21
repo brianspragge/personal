1c9a02847dcb5e32824636cecfa4ac659f0b88da
Brian Spragge <brianspragge@gmail.com>
2025-06-21 14:19:19 -0700

    auto ln with sync, keeping .rc files out of ~/ and instead in ~/personal.

---
+++ sync.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+ln -sf ~/.rc/.bashrc ~/.bashrc
+ln -sf ~/.rc/.clang-format ~/.clang-format
+ln -sf ~/.rc/.gpg_crd_cmds.txt ~/.gpg_crd_cmds.txt
+ln -sf ~/.rc/.gpg_crd.sh ~/.gpg_crd.sh
+ln -sf ~/.rc/.inputrc ~/.inputrc
+ln -sf ~/.rc/.vimrc ~/.vimrc
