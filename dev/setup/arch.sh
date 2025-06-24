c3c7986414c6409ccc4088d7e08d64f53cd5e85d
Brian Spragge <brianspragge@gmail.com>
2025-06-23 21:32:12 -0700

    did stuff and things.

--- dev/setup/arch.sh
+++ dev/setup/arch.sh
@@ -4,8 +4,8 @@
 # I made this so I don't need to find and manually install each.
 
 
-sudo pacman -S less
-sudo pacman -S nano vim git
+sudo pacman -S pigz pbzip2  # For AUR config(faster go brrr)
+sudo pacman -S nano vim git less
 sudo pacman -S brightnessctl playerctl
 sudo pacman -S chromium
 
