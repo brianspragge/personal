79105c9d11936ed0e24226b2792197bae6033e93
Brian Spragge <brianspragge@gmail.com>
2025-06-24 22:31:15 -0700

    more more more setupsss.

--- dev/setup/arch.sh
+++ dev/setup/arch.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
 
-# Installs the packages I like to have in Arch.
-# I made this so I don't need to find and manually install each.
-
+# This script installs the packages I like to have in Arch.
 mkdir ~/builds; cd ~/builds
 
-# Downloading/Installing AUR
+# Cloning/Making AUR
 sudo pacman -S git base-devel
 sudo git clone https://aur.archlinux.org/yay.git
 cd yay
 sudo makepkg -si
 
 sudo yay --repo -S pigz pbzip2  # For AUR config(faster go brrr)
+
+# Firmware: remove warning messages for x230 laptop Arch build 
+sudo yay --aur mkinitcpio
+sudo yay --aur linux-firmware-qlogic 
+sudo yay --aur ast-firmware 
+sudo yay --aur aic94xx-firmware 
+sudo yay --aur wd719x-firmware
+
 sudo yay --repo -S less man mandb man-pages glibc
 sudo yay --repo -S nano vim
 sudo yay --repo -S brightnessctl playerctl
 sudo yay --repo -S chromium
-
+sudo yay --repo -S pkgstats
